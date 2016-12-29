09dc268186bcc980a4de310a9771a026da383995
swheatley <shaylee1@me.com>
2016-12-28 17:24:48 -0700

    Initial Github profile components created

--- app/index.js
+++ app/index.js
@@ -1,14 +1,61 @@
+var USER_DATA = {
+  name: 'Shaylee Hansen',
+  username: 'swheatley',
+  image: 'https://avatars3.githubusercontent.com/u/10644939?v=3&u=de36c685aee3e947fe03c9cebe31b98b2c5df208&s=400'
+}
+
+
 var React = require('react')
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+/* React Components Acronym
+
+  Focused
+  Independant
+  Reusable
+  Small
+  Testable
+
+*/
+
+var ProfilePic = React.createClass({
   render: function(){
-    return(
-      <div>Hello Reactjs Program</div>
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
+  }
+});
+
+var ProfileLink= React.createClass({
+  render: function(){
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+        {this.props.username}
+        </a>
+
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function(){
+    return(
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    )
+  }
+})
 ReactDOM.render(
-  <HelloWorld />, document.getElementById('app')
+  <Avatar user={USER_DATA}/>, document.getElementById('app')
 );
