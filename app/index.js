50bad08c600e482a5a5ef25f176e2921dc11c156
swheatley <shaylee1@me.com>
2016-12-29 00:27:22 -0700

    Created Routes

--- app/index.js
+++ app/index.js
@@ -1,12 +1,7 @@
-var USER_DATA = {
-  name: 'Shaylee Hansen',
-  username: 'swheatley',
-  image: 'https://avatars3.githubusercontent.com/u/10644939?v=3&u=de36c685aee3e947fe03c9cebe31b98b2c5df208&s=400'
-}
-
 
 var React = require('react')
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
 /* React Components Acronym
 
@@ -18,44 +13,6 @@ var ReactDOM = require('react-dom');
 
 */
 
-var ProfilePic = React.createClass({
-  render: function(){
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
-  }
-});
-
-var ProfileLink= React.createClass({
-  render: function(){
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-        {this.props.username}
-        </a>
-
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function(){
-    return(
-      <div>{this.props.name}</div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function(){
-    return(
-      <div>
-        <ProfilePic imageUrl={this.props.user.image}/>
-        <ProfileName name={this.props.user.name}/>
-        <ProfileLink username={this.props.user.username}/>
-      </div>
-    )
-  }
-})
 ReactDOM.render(
-  <Avatar user={USER_DATA}/>, document.getElementById('app')
+  routes, document.getElementById('app')
 );
