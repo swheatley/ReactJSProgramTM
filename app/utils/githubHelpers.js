d3302bcacc2c25155723499e58cc546835afa8d9
swheatley <shaylee1@me.com>
2017-01-01 17:45:59 -0700

    Completed componentDidMount function

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,29 @@
+var axios = require('axios');
+
+var id = 'YOUR_CLIENT_ID';
+var sec = 'YOUR_SECRET_ID';
+var param = '?client_id=' + id + '&client_secrets=' + sec;
+
+
+function getUserInfo(username){
+  return axios.get('https://api.github.com/users/' + username + param);
+
+}
+var helpers = {
+  getPlayersInfo: function(players){
+    // fetch some data from Github
+    return axios.all(players.map(function(username){
+      return getUserInfo(username);
+    })).then(function(info){
+      return info.map(function(user){
+        return user.data;
+
+      })
+
+    }).catch(function(err){
+      console.err('Error in getPlayersInfo', err)
+    })
+  }
+};
+
+module.exports = helpers;
